57e8806a317bf044921942839c65fff5aaaa4731
Jeremy E Kozdon <jekozdon@nps.edu>
2020-04-29 10:26:28 -0700

    Slightly refactor test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,20 @@
 using Test
 using RootSolvers
 
+f(x) = x^2 - 100^2
+f′(x) = 2x
+
+# True root
+x̂ = 100
+@assert f(x̂) ≈ 0
+
 @testset "RootSolvers - compact solution correctness" begin
-    f(x) = x^2 - 100^2
-    f′(x) = 2x
     for FT in [Float32, Float64]
         sol =
             find_zero(f, FT(0.0), FT(1000.0), SecantMethod(), CompactSolution())
         @test sol.converged
         @test sol.root isa FT
-        @test sol.root ≈ 100
+        @test sol.root ≈ x̂
 
         sol = find_zero(
             f,
@@ -20,23 +25,21 @@ using RootSolvers
         )
         @test sol.converged
         @test sol.root isa FT
-        @test sol.root ≈ 100
+        @test sol.root ≈ x̂
 
         sol = find_zero(f, FT(1.0), NewtonsMethodAD(), CompactSolution())
         @test sol.converged
         @test sol.root isa FT
-        @test sol.root ≈ 100
+        @test sol.root ≈ x̂
 
         sol = find_zero(f, f′, FT(1.0), NewtonsMethod(), CompactSolution())
         @test sol.converged
         @test sol.root isa FT
-        @test sol.root ≈ 100
+        @test sol.root ≈ x̂
     end
 end
 
 @testset "RootSolvers - compact solution non-converged cases" begin
-    f(x) = x^2 - 100^2
-    f′(x) = 2x
     for FT in [Float32, Float64]
         sol = find_zero(
             f,
@@ -88,14 +91,12 @@ end
 end
 
 @testset "RootSolvers - verbose solution correctness" begin
-    f(x) = x^2 - 100^2
-    f′(x) = 2x
     for FT in [Float32, Float64]
         sol =
             find_zero(f, FT(0.0), FT(1000.0), SecantMethod(), VerboseSolution())
         @test sol.converged
         @test sol.root isa FT
-        @test sol.root ≈ 100
+        @test sol.root ≈ x̂
         @test sol.err < 1e-3
         @test sol.iter_performed < 20
         @test sol.iter_performed + 1 ==
@@ -111,7 +112,7 @@ end
         )
         @test sol.converged
         @test sol.root isa FT
-        @test sol.root ≈ 100
+        @test sol.root ≈ x̂
         @test sol.err < 1e-3
         @test sol.iter_performed < 20
         @test sol.iter_performed + 1 ==
@@ -121,7 +122,7 @@ end
         sol = find_zero(f, FT(1.0), NewtonsMethodAD(), VerboseSolution())
         @test sol.converged
         @test sol.root isa FT
-        @test sol.root ≈ 100
+        @test sol.root ≈ x̂
         @test sol.err < 1e-3
         @test sol.iter_performed < 20
         @test sol.iter_performed + 1 ==
@@ -131,7 +132,7 @@ end
         sol = find_zero(f, f′, FT(1.0), NewtonsMethod(), VerboseSolution())
         @test sol.converged
         @test sol.root isa FT
-        @test sol.root ≈ 100
+        @test sol.root ≈ x̂
         @test sol.err < 1e-3
         @test sol.iter_performed < 20
         @test sol.iter_performed + 1 ==
