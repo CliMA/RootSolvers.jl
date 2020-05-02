61fae630eb939ff5377a1fd277af62a1b4d981dc
Charles Kawczynski <kawczynski.charles@gmail.com>
2020-05-02 09:32:00 -0700

    Standardize API, improve cpu and gpu test format

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,159 +1,136 @@
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "env", "test"))
 using Test
 using RootSolvers
+using StaticArrays
 
-@testset "RootSolvers - compact solution correctness" begin
-    f(x) = x^2 - 100^2
-    f′(x) = 2x
+f(x) = x^2 - 100^2
+f′(x) = 2x
+x̃ = 100
+
+@testset "Scalar correctness test with compact solution" begin
+    F′ = typeof(f′)
     for FT in [Float32, Float64]
         for tol in
-            [ResidualTolerance(FT(1e-6)), SolutionTolerance(FT(1e-3)), nothing]
-            sol = find_zero(
-                f,
-                FT(0.0),
-                FT(1000.0),
-                SecantMethod(),
-                CompactSolution(),
-                tol,
-            )
-            @test sol.converged
-            @test sol.root isa FT
-            @test sol.root ≈ 100
+            [ResidualTolerance{FT}(1e-6), SolutionTolerance{FT}(1e-3), nothing]
+            for method in [
+                SecantMethod{FT}(0.0, 1000.0),
+                RegulaFalsiMethod{FT}(0.0, 1000.0),
+                NewtonsMethodAD{FT}(1.0),
+                NewtonsMethod{FT,F′}(1.0, f′)
+                ]
 
-            sol = find_zero(
-                f,
-                FT(0.0),
-                FT(1000.0),
-                RegulaFalsiMethod(),
-                CompactSolution(),
-                tol,
-            )
-            @test sol.converged
-            @test sol.root isa FT
-            @test sol.root ≈ 100
+                sol = find_zero(
+                    f,
+                    method,
+                    CompactSolution(),
+                    tol,
+                )
+                @test isbits(method)
+                @test sol.converged
+                @test sol.root isa FT
+                @test sol.root ≈ x̃
+            end
+        end
+    end
+end
 
-            sol =
-                find_zero(f, FT(1.0), NewtonsMethodAD(), CompactSolution(), tol)
-            @test sol.converged
-            @test sol.root isa FT
-            @test sol.root ≈ 100
+@testset "CPU Broadcast" begin
+    for FT in [Float32, Float64]
+        N = 5
+        X0 = rand(FT, N, N)
+        X1 = rand(FT, N, N) .+ 1000
 
-            sol = find_zero(
-                f,
-                f′,
-                FT(1.0),
-                NewtonsMethod(),
-                CompactSolution(),
-                tol,
-            )
-            @test sol.converged
-            @test sol.root isa FT
-            @test sol.root ≈ 100
+        for method in [
+            SecantMethod(X0, X1),
+            RegulaFalsiMethod(X0, X1),
+            NewtonsMethodAD(X0),
+            NewtonsMethod(X0, f′)
+            ]
+
+            sol = RootSolvers.find_zero.(Ref(f), method, CompactSolution())
+            converged = map(x->x.converged, sol)
+            X_roots = map(x->x.root, sol)
+            @test all(converged)
+            @test eltype(X_roots) == eltype(X0)
+            @test all(X_roots .≈ x̃)
+        end
+    end
+end
+
+@testset "CPU isbits Broadcast" begin
+    for FT in [Float32, Float64]
+        N = 5
+        X0 = SArray{Tuple{N,N}, FT}(rand(FT, N, N))
+        X1 = SArray{Tuple{N,N}, FT}(rand(FT, N, N)) .+ 1000
+
+        for method in [
+            SecantMethod(X0, X1),
+            RegulaFalsiMethod(X0, X1),
+            NewtonsMethodAD(X0),
+            NewtonsMethod(X0, f′)
+            ]
+
+            sol = RootSolvers.find_zero.(Ref(f), method, CompactSolution())
+            converged = map(x->x.converged, sol)
+            X_roots = map(x->x.root, sol)
+            @test isbits(method)
+            @test all(converged)
+            @test eltype(X_roots) == eltype(X0)
+            @test all(X_roots .≈ x̃)
         end
     end
 end
 
 @testset "RootSolvers - compact solution non-converged cases" begin
-    f(x) = x^2 - 100^2
-    f′(x) = 2x
+    F′ = typeof(f′)
     for FT in [Float32, Float64]
-        sol = find_zero(
-            f,
-            FT(0.0),
-            FT(1000.0),
-            SecantMethod(),
-            CompactSolution(),
-            SolutionTolerance(FT(1e-1)),
-            1,
-        )
-        @test !sol.converged
-        @test sol.root isa FT
+        for tol in
+            [ResidualTolerance{FT}(1e-6), SolutionTolerance{FT}(1e-1), nothing]
+            for method in [
+                SecantMethod{FT}(0.0, 1000.0),
+                RegulaFalsiMethod{FT}(0.0, 1000.0),
+                NewtonsMethodAD{FT}(1.0),
+                NewtonsMethod{FT,F′}(1.0, f′)
+                ]
 
-        sol = find_zero(
-            f,
-            FT(0.0),
-            FT(1000.0),
-            RegulaFalsiMethod(),
-            CompactSolution(),
-            SolutionTolerance(FT(1e-1)),
-            1,
-        )
-        @test !sol.converged
-        @test sol.root isa FT
-
-        sol = find_zero(
-            f,
-            FT(1.0),
-            NewtonsMethodAD(),
-            CompactSolution(),
-            SolutionTolerance(FT(1e-1)),
-            1,
-        )
-        @test !sol.converged
-        @test sol.root isa FT
-
-        sol = find_zero(
-            f,
-            f′,
-            FT(1.0),
-            NewtonsMethod(),
-            CompactSolution(),
-            SolutionTolerance(FT(1e-1)),
-            1,
-        )
-        @test !sol.converged
-        @test sol.root isa FT
+                sol = find_zero(
+                    f,
+                    method,
+                    CompactSolution(),
+                    tol,
+                    1,
+                )
+                @test !sol.converged
+                @test sol.root isa FT
+            end
+        end
     end
 end
 
 @testset "RootSolvers - verbose solution correctness" begin
-    f(x) = x^2 - 100^2
-    f′(x) = 2x
+    F′ = typeof(f′)
     for FT in [Float32, Float64]
-        sol =
-            find_zero(f, FT(0.0), FT(1000.0), SecantMethod(), VerboseSolution())
-        @test sol.converged
-        @test sol.root isa FT
-        @test sol.root ≈ 100
-        @test sol.err < 1e-3
-        @test sol.iter_performed < 20
-        @test sol.iter_performed + 1 ==
-              length(sol.root_history) ==
-              length(sol.err_history)
+        for method in [
+            SecantMethod{FT}(0.0, 1000.0),
+            RegulaFalsiMethod{FT}(0.0, 1000.0),
+            NewtonsMethodAD{FT}(1.0),
+            NewtonsMethod{FT,F′}(1.0, f′)
+            ]
 
-        sol = find_zero(
-            f,
-            FT(0.0),
-            FT(1000.0),
-            RegulaFalsiMethod(),
-            VerboseSolution(),
-        )
-        @test sol.converged
-        @test sol.root isa FT
-        @test sol.root ≈ 100
-        @test sol.err < 1e-3
-        @test sol.iter_performed < 20
-        @test sol.iter_performed + 1 ==
-              length(sol.root_history) ==
-              length(sol.err_history)
+            sol = find_zero(
+                f,
+                method,
+                VerboseSolution(),
+            )
+            @test sol.converged
+            @test sol.root isa FT
+            @test sol.root ≈ 100
+            @test sol.err < 1e-3
+            @test sol.iter_performed < 20
+            @test sol.iter_performed + 1 ==
+                  length(sol.root_history) ==
+                  length(sol.err_history)
+        end
 
-        sol = find_zero(f, FT(1.0), NewtonsMethodAD(), VerboseSolution())
-        @test sol.converged
-        @test sol.root isa FT
-        @test sol.root ≈ 100
-        @test sol.err < 1e-3
-        @test sol.iter_performed < 20
-        @test sol.iter_performed + 1 ==
-              length(sol.root_history) ==
-              length(sol.err_history)
-
-        sol = find_zero(f, f′, FT(1.0), NewtonsMethod(), VerboseSolution())
-        @test sol.converged
-        @test sol.root isa FT
-        @test sol.root ≈ 100
-        @test sol.err < 1e-3
-        @test sol.iter_performed < 20
-        @test sol.iter_performed + 1 ==
-              length(sol.root_history) ==
-              length(sol.err_history)
     end
 end
--- test/runtests_gpu.jl
+++ test/runtests_gpu.jl
@@ -1,25 +1,44 @@
 push!(LOAD_PATH, joinpath(@__DIR__, "..", "env", "GPU"))
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "env", "test"))
 
 using Test
 using RootSolvers
 
+using StaticArrays
+using Adapt
 using CUDAdrv
 using CUDAnative
 using CuArrays
 
 CuArrays.allowscalar(false)
-
 @testset "GPU tests" begin
+    f(x) = x^2 - 100^2
+    f′(x) = 2x
+    x̃ = 100
     for FT in [Float32, Float64]
-        X0 = cu(rand(FT, 5,5))
-        X1 = cu(rand(FT, 5,5)) .+ 1000
-        f(x) = x^2 - 100^2
 
-        sol = RootSolvers.find_zero.(f, X0, X1, SecantMethod(), CompactSolution())
-        converged = map(x->x.converged, sol)
-        X_roots = map(x->x.root, sol)
-        @test all(converged)
-        @test eltype(X_roots) == eltype(X0)
-        @test all(X_roots .≈ 100)
+        # Make isbits array, so that `method` remains isbits
+        N = 5
+        _X0 = SArray{Tuple{N,N}, FT}(rand(FT, N, N))
+        _X1 = SArray{Tuple{N,N}, FT}(rand(FT, N, N) .+ 1000)
+
+        # Move to the GPU
+        X0 = adapt(CuArray, _X0)
+        X1 = adapt(CuArray, _X1)
+
+        for method in [
+            SecantMethod(X0, X1),
+            RegulaFalsiMethod(X0, X1),
+            NewtonsMethodAD(X0),
+            NewtonsMethod(X0, f′)
+            ]
+
+            sol = RootSolvers.find_zero.(Ref(f), method, CompactSolution())
+            converged = map(x->x.converged, sol)
+            X_roots = map(x->x.root, sol)
+            @test all(converged)
+            @test eltype(X_roots) == eltype(X0)
+            @test all(X_roots .≈ 100)
+        end
     end
 end
